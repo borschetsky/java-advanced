7a3412a9f193f35f7c917b1c5dea80ba96af03e9
Viacheslav Moshkin <wod.moshkin@gmail.com>
2020-04-05 13:37:25 +0300

    Initial

--- src/com/week1/reflection/annotation/core/Test.java
+++ src/com/week1/reflection/annotation/core/Test.java
@@ -7,7 +7,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD) //can use in method only.
+@Target(ElementType.METHOD)
 public @interface Test {
     public boolean enabled() default true;
 }
--- src/com/week1/reflection/annotation/core/TesterInfo.java
+++ src/com/week1/reflection/annotation/core/TesterInfo.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE) //on class level
+@Target(ElementType.TYPE) 
 public @interface TesterInfo {
 
     public enum Priority {
