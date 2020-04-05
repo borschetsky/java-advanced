1649cc9d93c0ac8c722bd350d78097a8be106371
Viacheslav Moshkin <wod.moshkin@gmail.com>
2020-04-05 13:34:48 +0300

    Initial commit; INFM205 - Week 1 Reflection;

---
+++ src/com/week1/reflection/annotation/TestExample.java
@@ -0,0 +1,42 @@
+package com.week1.reflection.annotation;
+
+import com.company.Math;
+import com.week1.reflection.annotation.core.Test;
+import com.week1.reflection.annotation.core.TesterInfo;
+import com.week1.reflection.annotation.core.TesterInfo.Priority;
+
+@TesterInfo(
+        priority = Priority.HIGH,
+        createdBy = "Viacheslav MNoshkin",
+        tags = {"infm205","test" }
+)
+public class TestExample {
+
+    @Test
+    void testA() {
+        if (true)
+            throw new RuntimeException("This test always failed");
+    }
+
+    @Test(enabled = false)
+    void testB() {
+        if (false)
+            throw new RuntimeException("This test always passed");
+    }
+
+    @Test(enabled = true)
+    void testC() {
+        if (10 > 1) {
+            // do nothing, this test always passed.
+        }
+    }
+
+    @Test(enabled = true)
+    void testD() {
+        int sum = Math.sum(5, 5);
+        if(sum != 10) {
+            throw  new RuntimeException("Test should return correct sum");
+        }
+    }
+
+}
