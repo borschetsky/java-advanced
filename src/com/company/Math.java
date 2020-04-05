3bd89f4bcabbaa75653b8760846b29e7a93a3e7e
Viacheslav Moshkin <wod.moshkin@gmail.com>
2020-04-05 13:34:48 +0300

    Initial commit; INFM205 - Week 1 Reflection;

---
+++ src/com/company/Math.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Math {
+    //You can modify this method due to return incorrect sum
+    public static int sum(int a, int b) {
+        return a + b ;
+    }
+
+}
